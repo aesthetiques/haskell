2cffaa9df7e34145ba3304e98895ed9e1fb497c2
aesthetiques <caleb.wells1@gmail.com>
2018-09-26 17:38:20 -0700

    playing with iteration

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,8 +2,11 @@ module Lib
     ( 
         someFunc, 
         doubleIt,
-        doubleRange
+        doubleRange,
+        rotateList
     ) where
+
+import Data.List
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
@@ -13,3 +16,12 @@ doubleIt x = x * 2
 
 doubleRange :: Int -> [Int]
 doubleRange x = [x * 2 | x <- [1..10]]
+
+rotateList :: Int -> [a] -> [a]
+rotateList rotations list = zipWith const (drop rotations (cycle list)) list
+
+rotateList' :: Int -> [a] -> [a]
+rotateList' rotations list = take (length list) $ drop rotations (cycle list)
+
+modCheck x = filter (test) x
+    where test x = mod x 2 == 0
